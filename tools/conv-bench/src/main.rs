dd6284e99c7037e52ee853e0cabe2390bc6e327c
snjax <igor.gulamov@gmail.com>
2024-03-20 21:11:08 +0400

    Added benchmarking tool for convolution
    This update introduces a new benchmarking tool for convolution operations. The tool generates random data and uses the rayon library to parallelize the computation of convolutions, measuring the time taken for both Goldilocks and u64 types. This will help in performance testing and optimization efforts.

---
+++ tools/conv-bench/src/main.rs
@@ -0,0 +1,62 @@
+use p3_goldilocks::Goldilocks;
+use rand::{random};
+use rayon::prelude::*;
+use std::hint::black_box;
+
+fn main() {
+    
+    const N:usize = 32;
+    const L:usize = 1<<24;
+    const K:usize = 1<<8;
+
+
+    let data: Vec<(Vec<Goldilocks>,Vec<Goldilocks>)> = (0..N).map(|_| 
+        ((0..L).map(|_| random()).collect(),(0..L+K).map(|_| random()).collect())  
+    ).collect();
+
+
+    let start = std::time::Instant::now();
+    let data = black_box(data);
+    let res = data.par_iter().map(|(a,b)| {
+        let mut sum = Goldilocks::default();
+        for k in 0..K {
+            for i in 0..L {
+                sum += a[i] * b[i+k];
+            }
+        }
+        sum
+    }).collect::<Vec<Goldilocks>>();
+    black_box(res);
+    let elapsed = start.elapsed();
+
+    let time = elapsed.as_secs() as f64 + f64::from(elapsed.subsec_nanos()) * 1e-9;
+
+    println!("Bitrate for Goldilocks: {:.2} GB/s", 2.0 * K as f64 * N as f64 * L as f64 * std::mem::size_of::<Goldilocks>() as f64 / time / 1e9);
+    
+
+    let data: Vec<(Vec<u64>,Vec<u64>)> = (0..N).map(|_| 
+        ((0..L).map(|_| random()).collect(),(0..L+K).map(|_| random()).collect())  
+    ).collect();
+
+
+    let start = std::time::Instant::now();
+    let data = black_box(data);
+    let res = data.par_iter().map(|(a,b)| {
+        let mut sum = u64::default();
+        for k in 0..K {
+            for i in 0..L {
+                sum += a[i] * b[i+k];
+            }
+        }
+        sum
+    }).collect::<Vec<u64>>();
+    black_box(res);
+    let elapsed = start.elapsed();
+
+    let time = elapsed.as_secs() as f64 + f64::from(elapsed.subsec_nanos()) * 1e-9;
+
+    println!("Bitrate for u64: {:.2} GB/s", 2.0 * K as f64 * N as f64 * L as f64 * std::mem::size_of::<Goldilocks>() as f64 / time / 1e9);
+    
+
+
+}
